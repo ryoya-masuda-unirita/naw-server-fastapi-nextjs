d3f317399f006b69486f913cadee8d2a2ffff519
ryoya-masuda-unirita <ryouya_masuda@unirita.co.jp>
2026-07-18 17:33:59 +0900

    #160 issue-160 allowed_ssh_cidrs変数を追加     - variables.tfに踏み台SSH許可元CIDR用の変数（list(string)、デフォルトなし）を追加     - terraform.tfvars.exampleに記入例を追加     - terraform plan がエラーなく完了することを確認

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,6 +3,7 @@ variable "region" {
   default = "ap-northeast-1"
 }
 
+// デフォルトがあればそのまま使える｡デフォルトがなければ､terraform.tfvarsで指定する必要がある｡
 variable "project_name" {
   type    = string
   default = "naw-fastapi-issue160"
@@ -13,3 +14,7 @@ variable "db_password" {
   sensitive = true
 }
 
+variable "allowed_ssh_cidrs" {
+  type        = list(string)
+  description = "踏み台へのSSH接続を許可するCIDRブロックのリスト"
+}
